3abd2335f17fdbdff8e0b0dd1d6c71850d93a1f5
Marcio <mecastillo00@gmail.com>
2020-10-07 15:26:24 -0400

    Updates

---
+++ precache-manifest.c1274d4fde1e82fbb1b01d54bdede349.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68eadbd0e79951e128d49e91d94d0a5",
+    "url": "/trivia-questions/index.html"
+  },
+  {
+    "revision": "37aa4b291672df8bf495",
+    "url": "/trivia-questions/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "148cf6aeddc3781dd992",
+    "url": "/trivia-questions/static/css/main.736d664c.chunk.css"
+  },
+  {
+    "revision": "37aa4b291672df8bf495",
+    "url": "/trivia-questions/static/js/2.5602a462.chunk.js"
+  },
+  {
+    "revision": "e706b8ea620267704a943351d7a126f0",
+    "url": "/trivia-questions/static/js/2.5602a462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148cf6aeddc3781dd992",
+    "url": "/trivia-questions/static/js/main.8cbef42a.chunk.js"
+  },
+  {
+    "revision": "197d7dab23b084562104",
+    "url": "/trivia-questions/static/js/runtime-main.b5426d29.js"
+  },
+  {
+    "revision": "7a3fb46ec2414ffa8460c1b698dd5625",
+    "url": "/trivia-questions/static/media/error.7a3fb46e.svg"
+  },
+  {
+    "revision": "462972c5861eb9fcb4779d61bf45cbb6",
+    "url": "/trivia-questions/static/media/question.462972c5.svg"
+  },
+  {
+    "revision": "c3fc9fc32fd354c614f020c84b310002",
+    "url": "/trivia-questions/static/media/trivia-image.c3fc9fc3.jpg"
+  }
+]);
